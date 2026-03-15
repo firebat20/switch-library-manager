61be9b825a00f0e5963939fca85fcdaf489e6a85
Daniel Lorenzo <daniel@lorenzo.email>
2026-03-15 22:22:34 +0100

    fix: only remove old updates if they are also not a "base" game

--- src/db/localSwitchFilesDB.go
+++ src/db/localSwitchFilesDB.go
@@ -270,22 +270,21 @@ func (ldb *LocalSwitchDBManager) processLocalFiles(files []ExtendedFileInfo,
 				if metadata.Version > switchTitle.LatestUpdate {
 					if switchTitle.LatestUpdate != 0 {
 						oldUpdate := switchTitle.Updates[switchTitle.LatestUpdate]
-						// Check if the old update is part of the base game file
-                        if switchTitle.BaseExist &&
-                            oldUpdate.ExtendedInfo.BaseFolder == switchTitle.File.ExtendedInfo.BaseFolder &&
-                            oldUpdate.ExtendedInfo.FileName == switchTitle.File.ExtendedInfo.FileName {
-                            // The old update is part of the base game file.
-                            // Don't skip the file, just remove the outdated update version from the map.
-                            delete(switchTitle.Updates, switchTitle.LatestUpdate)
-                        } else {
-                            // The old update is a separate file, so flag it as old.
-                            skipped[oldUpdate.ExtendedInfo] = SkippedFile{ReasonCode: REASON_OLD_UPDATE, ReasonText: "old update file, newer update exist locally\nnew: " + filepath.Join(file.BaseFolder, file.FileName) + "\nold:" + filepath.Join(oldUpdate.ExtendedInfo.BaseFolder, oldUpdate.ExtendedInfo.FileName)}
-                        }
+						if switchTitle.BaseExist &&
+							oldUpdate.ExtendedInfo.BaseFolder == switchTitle.File.ExtendedInfo.BaseFolder &&
+							oldUpdate.ExtendedInfo.FileName == switchTitle.File.ExtendedInfo.FileName {
+							delete(switchTitle.Updates, switchTitle.LatestUpdate)
+						} else {
+							skipped[oldUpdate.ExtendedInfo] = SkippedFile{ReasonCode: REASON_OLD_UPDATE, ReasonText: "old update file, newer update exist locally\nnew: " + filepath.Join(file.BaseFolder, file.FileName) + "\nold:" + filepath.Join(oldUpdate.ExtendedInfo.BaseFolder, oldUpdate.ExtendedInfo.FileName)}
+						}
 					}
 					switchTitle.LatestUpdate = metadata.Version
 				} else {
-					newerUpdate := switchTitle.Updates[switchTitle.LatestUpdate]
-					skipped[file] = SkippedFile{ReasonCode: REASON_OLD_UPDATE, ReasonText: "old update file, newer update exist locally\nnew: " + filepath.Join(newerUpdate.ExtendedInfo.BaseFolder, newerUpdate.ExtendedInfo.FileName) + "\nold:" + filepath.Join(file.BaseFolder, file.FileName)}
+					// Flag only if this file is not also considered the base game
+					if !switchTitle.BaseExist || file.BaseFolder != switchTitle.File.ExtendedInfo.BaseFolder || file.FileName != switchTitle.File.ExtendedInfo.FileName {
+						newerUpdate := switchTitle.Updates[switchTitle.LatestUpdate]
+						skipped[file] = SkippedFile{ReasonCode: REASON_OLD_UPDATE, ReasonText: "old update file, newer update exist locally\nnew: " + filepath.Join(newerUpdate.ExtendedInfo.BaseFolder, newerUpdate.ExtendedInfo.FileName) + "\nold:" + filepath.Join(file.BaseFolder, file.FileName)}
+					}
 				}
 				continue
 			}
